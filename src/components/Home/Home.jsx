3b708c610792952ca153548ea565d11b2d14d7b3
jabss <jabrijuhinin93@gmail.com>
2025-12-31 17:25:47 +0800

    home component design

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,25 @@
+import Grid from "@mui/material/Grid";
+import MyFavouritesPanel from "../MyFavouritePanel/MyFavouritePanel";
+
 const Home = () => {
     return (
-        <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the home page of the application.</p>
-        </div>
+        <Grid container className="home-container" direction={"column"}>
+            <Grid className="header-container" item lg={1} style={{maxheight: "10vh"}}> </Grid>
+            <Grid className="content-container" item lg={11}>
+                <Grid container direction="row" style={{ height: "100%"}}>
+                    <Grid className="left-panel-container" item lg="2.5">
+                        <MyFavouritesPanel style={{ overflowY: "scroll"}}
+                            handleSetKeyword={handleSetKeyword}
+                            myFavourites={myFavourites}
+                            clearmyFavourites={clearmyFavourites}
+                        ></MyFavouritesPanel>
+                    </Grid>
+                    <Grid className="result-container" item lg="9.5">
+                        <DisplayResults keyWord={keyWord} updatemyFavourites={updatemyFavourites}></DisplayResults>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
